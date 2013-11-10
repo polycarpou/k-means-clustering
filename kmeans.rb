d4632b650eafce72af45c46ce20777fb049104f6
Michael Polycarpou <michaelpolycarpou@gmail.com>
2013-11-10 14:29:01 -0500

    writing a k means clustering algorithm, works for the 2d case

---
+++ kmeans.rb
@@ -0,0 +1,162 @@
+require 'pry'
+#require 'gruff'
+small_map = 
+[
+  [" "," ","█","█"," "],
+  [" "," "," "," ","█"],
+  [" ","█"," ","█","█"],
+  ["█","█"," "," "," "],
+  ["█","█"," ","█"," "]
+]
+map = 
+[
+  ["-","-","-","-","-"],
+  ["-","-","-","-","-"],
+  ["-","-","-","-","-"],
+  ["-","-","-","-","-"],
+  ["-","-","-","-","-"]
+]
+# Coordinates
+#        [0,0][1,0][2,0]-->
+# [0,0]  [" "," ","█","█"," "],
+# [0,1]  [" "," "," "," ","█"],
+# [0,2]  [" ","█"," ","█","█"],
+#   |    ["█","█"," "," "," "],
+#   V    ["█","█"," ","█"," "]
+
+# input:
+#   E = e1,e2,e3 ... set of entities to be clustered
+#   k = number of clusters
+#   MaxIters = limit of iterations
+# output:
+#   C = c1,c2,c3,c4 .. set of cluster centroids
+#   L = set of cluster labels of E
+
+# foreach c 
+#   randomly assign e to a cluster
+# end
+
+# foreach e
+#   l(e) <-- argmin distance to each c
+# end
+
+# changed false
+# iter 0
+# do 
+#   foreach e do
+#     update cluster(ci)
+#   end
+
+#   for each e do
+#     minDist <-- argminDistance(ei,cj) j for 1..k
+#     if minDist != l(e) then
+#       l(ei) <- minDist
+#       changed <- true
+#     end
+#   end
+#   iter += 1
+
+# until changed == true and iter <= MaxIters
+class KMeans
+  attr_accessor :map, :points, :assignments, :centers
+  def initialize(map)
+    @map = map
+  end
+  def for_print
+    self.map.each do |row|
+      row.each do |box|
+        print box
+      end
+      puts
+    end
+  end
+
+  def mappoints(points)
+    points.each do |point|
+      x = point[0]
+      y = point[1]
+      self.map[x][y] = "o"
+    end
+    for_print
+  end
+
+  def calculate_new_centers(k)
+    self.centers.map do |group|
+      group_arr = assignments.select do |assignment|
+        assignment[1] == group
+      end
+      sum_x = 0
+      sum_y = 0
+      group_arr.each do |point|
+        sum_x += point[0][0]
+        sum_y += point[0][1]
+      end
+      if group_arr.count != 0
+        mean_x = sum_x/group_arr.count
+        mean_y = sum_y/group_arr.count
+      end
+      [mean_x, mean_y]
+    end
+  end
+
+  def distance(center, point)
+    (center[0] - point[0])**2 + (center[1] - point[1])**2
+  end
+
+  def reassign_groups(centers)
+   #old_assignments = assignments
+   #p "old_assignments #{assignments}"
+    assignments.each do |assignment|
+      point = assignment[0]
+      new_group = centers.min_by do |center|
+        distance(center,point)
+      end
+      assignment[1] = new_group
+    end
+   #old_assignments.sort != assignments.sort #changed?
+  end
+
+
+  def cluster(k,points,max_iters)
+    self.assignments = []
+
+    self.centers = [[1,1],[1,2]]
+    points.each do |point|
+      self.assignments << [point, centers[rand(0..centers.count-1)]]
+    end
+    p assignments
+
+    changed = false
+    iter = 0
+    #5.times do
+    begin 
+      self.centers = calculate_new_centers(k)
+      reassign_groups(centers)
+      iter += 1
+    end  while iter <= max_iters
+
+  
+  groups = self.centers.map do |center|
+    self.assignments.select {|assignment| assignment[1] == center}
+  end
+  groups = groups.collect {|group| group.collect{|x| x[0] }   }
+  p "These are the groups"
+  p groups
+  binding.pry
+
+  end
+end
+
+ algo = KMeans.new(map)
+ points = [[4,2],[4,3],[4,4],[1,2],[1,3],[1,1],[3,2]]
+# algo.cluster(2,points,6)
+# algo.map = map
+# points = [[0,0],[1,0],[4,4],[4,3],[1,1],[3,4]]
+# algo.mappoints(points)
+# # algo.cluster(3,points, 5)
+
+# #algo.calculate_new_centers(3)
+ algo.cluster(2,points, 5)
+ p algo.centers
+ p algo.assignments
+# p algo.assignments
---
+++ kmeans_spec.rb
@@ -0,0 +1,55 @@
+require_relative 'spec_helper.rb'
+require_relative 'kmeans.rb'
+
+
+map = [
+[" "," ","█"," "," "],
+[" "," "," "," ","█"],
+[" ","█","█","█","█"],
+[" ","♥"," "," "," "],
+[" "," "," ","█"," "]
+]
+
+describe KMeans do
+  let(:new_cluster) { KMeans.new(map) }
+  context "#new(map)" do
+    it "instantiates a new map without errors" do   
+      expect(new_cluster.map).to eq(map)
+    end
+  end
+    
+  context "#distance(center,point)" do
+    it "gives the euclidean distance of the given point and center" do
+      new_cluster.distance([0,0],[-3,-4]).should eq(25)
+      new_cluster.distance([5,12],[0,0]).should eq(13**2)
+    end
+  end
+
+  context "#calculate_new_centers(k)" do
+	  it "given the points separated into their respective groups we find the center of these groups" do
+     new_cluster.centers = [[4,4],[4,3],[4,2]] # previous centers
+	   new_cluster.assignments = [[[4,2],[4,3]], [[4,3],[4,3]],[[4,4],[4,3]], [[1,2],[4,4]],[[1,3],[4,4]],[[1,1],[4,4]],[[3,2],[4,2]]]
+     new_cluster.calculate_new_centers(2).sort.should eq([[4,3],[3,2],[1,2]].sort)
+	  end
+  end
+
+  context "#reassign_groups(centers)" do
+    it "it takes several center points and puts the rest of the points into the nearest group" do
+	    new_cluster.assignments = [[[4,2],2],[[4,3],1],[[4,4],2],[[1,2],2],[[1,3],1],[[1,1],3],[[3,2],2]]
+      correct_assignments = [[[4,2],[4,3]], [[4,3],[4,3]],[[4,4],[4,3]], [[1,2],[0,2]],[[1,3],[0,2]],[[1,1],[0,2]],[[3,2],[4,3]]]
+     #p new_cluster.reassign_groups([[1,2],[4,3]])
+      new_cluster.reassign_groups([[0,2],[4,3]])
+      new_cluster.assignments.sort.should eq(correct_assignments.sort)
+     #p new_cluster.reassign_groups([[0,2],[4,3]])
+      
+    end
+  end
+
+  context "#cluster(starting_node, looking_for" do
+    it "implements the k-means clustering algorithm, takes the data points and allocates them into k groups of similar elements" do
+     #points = [[4,2],[4,3],[4,4],[1,2],[1,3],[1,1],[3,2]]
+     #p new_cluster.cluster(2,points,6)
+    end
+  end
+end 
+
